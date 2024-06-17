363cbf7ac55bffa9ff2d118797e8564cd6e76506
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-06-17 22:52:12 +0000

    ncommit

--- tw-spacing/output/responsive_scale/border_radius_values.dart
+++ tw-spacing/output/responsive_scale/border_radius_values.dart
@@ -1,10 +1,14 @@
 part of 'border_radius.dart';
 
-BorderRadius roundedXL() => rounded(24);
-
+// roundedSM => border radius = 8px 
 BorderRadius roundedSM() => rounded(8);
 
+// roundedMD => border radius = 12px 
 BorderRadius roundedMD() => rounded(12);
 
+// roundedLG => border radius = 16px 
 BorderRadius roundedLG() => rounded(16);
+
+// roundedXL => border radius = 24px 
+BorderRadius roundedXL() => rounded(24);
 
--- tw-spacing/output/responsive_scale/radius_values.dart
+++ tw-spacing/output/responsive_scale/radius_values.dart
@@ -1,10 +1,14 @@
 part of 'radius.dart';
 
-Radius rXL() => r(24);
-
+// rSM => radius = 8px 
 Radius rSM() => r(8);
 
+// rMD => radius = 12px 
 Radius rMD() => r(12);
 
+// rLG => radius = 16px 
 Radius rLG() => r(16);
+
+// rXL => radius = 24px 
+Radius rXL() => r(24);
 
